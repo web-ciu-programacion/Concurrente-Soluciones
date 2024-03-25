3a77fe81254a55e792c907b92d88a7186e066eae
acelay <acelay@nacionservicios.com.ar>
2024-03-25 20:25:47 -0300

    ejercicio 9 capitulo 4

---
+++ capitulo-4-ejercicio-9/src/ciu/concurrencia/semaphore/Auto.java
@@ -0,0 +1,46 @@
+package ciu.concurrencia.semaphore;
+
+public class Auto implements Runnable {
+
+	private Sentido sentido;
+
+	public Auto(Sentido sentido) {
+		super();
+		this.sentido = sentido;
+	}
+
+	@Override
+	public void run() {
+		Thread t = Thread.currentThread();
+		try {
+			if (Sentido.NORTE_SUR.equals(this.sentido)) {
+				System.out.println(t.getName() + " avanza en sentido: " + this.sentido.name() + " en tramo: 1");
+				Shared.puenteNorte.acquire();
+				System.out.println(t.getName() + " cruzo el puente Norte");
+				Shared.puenteNorte.release();
+				System.out.println(t.getName() + " avanza en sentido: " + this.sentido.name() + " en tramo: 2");
+				Shared.puenteSur.acquire();
+				System.out.println(t.getName() + " cruzo el puente Sur");
+				Shared.puenteSur.release();
+				System.out.println(t.getName() + " cruzo los 2 puentes y continua rumbo al Sur");
+			} else {
+				System.out.println(t.getName() + " avanza en sentido: " + this.sentido.name() + " en tramo: 1");
+				Shared.puenteSur.acquire();
+				System.out.println(t.getName() + " cruzo el puente Sur");
+				Shared.puenteSur.release();
+				System.out.println(t.getName() + " avanza en sentido: " + this.sentido.name() + " en tramo: 2");
+				Shared.puenteNorte.acquire();
+				System.out.println(t.getName() + " cruzo el puente Norte");
+				Shared.puenteNorte.release();
+				System.out.println(t.getName() + " cruzo los 2 puentes y continua rumbo al Norte");
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Sentido getSentido() {
+		return sentido;
+	}
+
+}
